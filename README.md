fb0b3199209c354dc2f7accdfcbac1ad92382015
kuzi-moto <kuzimoto@gmail.com>
2019-09-25 10:50:45 -0500

    Add readme, firefox start headless

---
+++ README.md
@@ -0,0 +1,5 @@
+# Setup
+
+1. `npm install`
+2. Install appropriate drivers
+    * Firefox [Gecko Driver](https://github.com/mozilla/geckodriver/releases)
--- index.js
+++ index.js
@@ -1,33 +1,20 @@
-var webdriver = require('selenium-webdriver'),
-    By = webdriver.By,
-    until = webdriver.until;
+const {Builder, By, Key, until} = require('selenium-webdriver');
+const firefox = require('selenium-webdriver/firefox');
 
-var firefox = require('selenium-webdriver/firefox');
-var options = new firefox.Options();
-options.addArguments("-headless");
+let options = new firefox.Options()
+    .headless();
 
-var driver = new webdriver.Builder()
+let driver = new Builder()
     .forBrowser('firefox')
     .setFirefoxOptions(options)
     .build();
 
-driver.get('https://www.google.com');
-driver.findElement(By.name('q')).sendKeys('webdriver');
-
-driver.sleep(1000).then(function() {
-    driver.findElement(By.name('q')).sendKeys(webdriver.Key.TAB);
-});
-
-driver.findElement(By.name('btnK')).click();
-
-driver.sleep(2000).then(function() {
-    driver.getTitle().then(function(title) {
-        if (title === 'webdriver - Google Search') {
-            console.log('Test passed');
-        } else {
-            console.log('Test failed');
-        }
-    });
-});
-
-driver.quit();
+(async function example() {
+  try {
+    await driver.get('http://www.google.com/ncr');
+    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+    await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+  } finally {
+    await driver.quit();
+  }
+})();
